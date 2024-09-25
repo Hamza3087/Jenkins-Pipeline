7d29b9747ad9e604a9f7dc949324ac7735d42e92
hamza_tariq <hamzagondal921@gmail.com>
2024-09-25 21:31:01 +0500

    commit

--- prime.py
+++ prime.py
@@ -1,9 +1,9 @@
 def is_prime(n):
     """Check if a number is prime."""
     if n <= 1:
-        return False
+        return 0
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            return False
-    return True
+            return 0
+    return 1
 
